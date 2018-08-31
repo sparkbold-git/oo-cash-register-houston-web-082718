df9949516cc56e68982d389028ba934b2c999b51
Trung Hoang <sparkboldstudio@gmail.com>
2018-08-31 09:26:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
 
   end
   
-  def apply_discount(discount)
-      @total -= @total * discount if discount > 0
+  def apply_discount
+      @total -= @total * @discount if @discount > 0
   end
 end
